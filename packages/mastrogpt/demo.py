606b71b8e61d65e62cf48da3a7f134fd6c180b84
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2024-04-26 17:27:02 +0100

    remove tests

--- packages/mastrogpt/demo.py
+++ packages/mastrogpt/demo.py
@@ -1,19 +1,11 @@
 #--web true
 def main(args):
 
-    # 1a read the input field from args in input, return '' as default
-    input = args.get("input", "")
-    
-    #
+    ## 1a read the input field from args in input, return '' as default
         
     # produce output from input with a suitable default
     input = "nothing" if input == "" else input
     output = { "output": f"You said: {input}" }
 
-    # 1b return a dictionary with key 'body' and value 
-    return {
-        "body": output 
-    }
-    
-    #
+    ## 1b return a dictionary with key 'body' and value 
     
