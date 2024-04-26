97fc912e2720b3bbca310cd844c92f9ef6ed23fc
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2024-04-26 12:07:54 +0100

    fixes

--- packages/mastrogpt/demo.py
+++ packages/mastrogpt/demo.py
@@ -1,11 +1,17 @@
 #--web true
 def main(args):
 
-    ## 1a read the input field from args in input, return '' as default
+    # read the input field from args in input, return '' as default
+    input = args.get("input", "")
+    
         
     # produce output from input with a suitable default
     input = "nothing" if input == "" else input
     output = { "output": f"You said: {input}" }
 
-    ## 1b return a dictionary with key 'body' and value output
+    # return a dictionary with key 'body' and value 
+    return {
+        "body": output 
+    }
+    
     
--- tests/test_1.py
+++ tests/test_1.py
@@ -8,13 +8,13 @@ def test_demo():
     # {
     #   "body": { "output": "You said: nothing" }
     # }
-    assert demo.main({})["body"] == "You said: nothing"
+    assert demo.main({})["body"]["output"] == "You said: nothing"
 
     # expected result:
     # {
     #   "body": { "output": "You said: hello" }
     # }
-    assert demo.main({"input": "hello"})["body"] == "You said: hello"
+    assert demo.main({"input": "hello"})["body"]["output"] == "You said: hello"
 
 def test_index():
     # expected result:
