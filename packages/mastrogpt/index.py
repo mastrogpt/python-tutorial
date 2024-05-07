ea7f5b8d185b2ed9f1b8e03e64ddf0ecc4118bfb
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2024-05-07 08:08:03 +0100

    fix test

--- packages/mastrogpt/index.py
+++ packages/mastrogpt/index.py
@@ -6,7 +6,7 @@ def main(arg):
         "services": [
             ## 1c add the demo entry, an object with name Demo and url mastrogpt/demo
             
-            ## 2c add the OpenAI entry, an object with name Demo and url mastrogpt/demo
+            ## 2c add the OpenAI entry, an object with name OpenAI and url openai/chat
         ]
     }
     return {"body": data}
